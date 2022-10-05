78fd93fdfe743a6a188fdc030053bdc4a8590d6f
Alfonso Bravo <alfonso.bravo.molina@gmail.com>
2022-10-05 23:34:22 +0200

    Add Header info. Add css for header.

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,11 +1,15 @@
 const Header = ({selectedTeam, teamMemberCount}) => {
 
-    return(
-        <div>
-            <h1>Team Member Allocation</h1>
-            <h3>{selectedTeam} has {teamMemberCount} Members</h3>
+  return(
+    <div className="container">
+      <div className="row justify-content-center mt-3 mb-4">
+        <div className="col-8">
+          <h1>Team Member Allocation</h1>
+          <h3>{selectedTeam} has {teamMemberCount} Members</h3>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Header
