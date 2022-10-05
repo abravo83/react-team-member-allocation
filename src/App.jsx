20f6cdf2fc167aa7723aa0f929db7255a671b14b
Alfonso Bravo <alfonso.bravo.molina@gmail.com>
2022-10-05 12:26:07 +0200

    Create Employees.jsx => Include data as local state | Include in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import './App.css';
 import Header from './Header'
 import Content from './Content'
 import Footer from './Footer'
+import Employees from './Employees';
 
 function App() {
   return (
     <main>
       <Header />
+      <Employees />
       <Content />
       <Footer />
     </main>
