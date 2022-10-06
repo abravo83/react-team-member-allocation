966f7a6b7847b1760eb231d571871ab718296e40
Alfonso Bravo <alfonso.bravo.molina@gmail.com>
2022-10-06 06:45:33 +0200

    Add var today + HTML to Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,7 +1,15 @@
 const Footer = () => {
 
+    var today= new Date();
+
     return(
-        <div>Footer</div>
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-8">
+                    <h5>Team Member Allocation App - {today.getFullYear()}</h5>
+                </div>
+            </div>
+        </div>
     );
 }
 
